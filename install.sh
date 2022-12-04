c9cf15cc0c889753fcc99639ca55cca103ead374
Alvaro Candela <alvarocandela47@gmail.com>
2022-12-04 22:39:01 +0100

    Updated install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ paru -S --noconfirm - < pkglist.txt
 
 stow bspwm coc fondos kitty nvim p10k picom polybar rofi startpage studymode sxhkd xinitrc zathura zsh
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-nvim --headless +PackerSync +qall
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 curl -fsSL https://raw.githubusercontent.com/spicetify/spicetify-cli/master/install.sh | sh
 curl -fsSL https://raw.githubusercontent.com/spicetify/spicetify-marketplace/main/resources/install.sh | sh
 spicetify -n apply
--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,5 +1,18 @@
-return require('packer').startup(function()
-	use 'wbthomason/packer.nvim'
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
 
 	-- Colorscheme
 	use 'navarasu/onedark.nvim'
@@ -41,4 +54,7 @@ return require('packer').startup(function()
     require("mason").setup()
     require'lspconfig'.pylsp.setup{} 
     use {'neoclide/coc.nvim', branch = 'release'}
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
