4d23c3f35f78d135d49eb628651194182fcc612e
Alvaro Candela <alvarocandela47@gmail.com>
2023-02-24 22:54:21 +0100

    Removed install scripts that did not work properly

--- install.sh
+++
@@ -1,8 +0,0 @@
-paru -S --noconfirm - < pkglist.txt
-
-stow bspwm coc fondos kitty nvim p10k picom polybar rofi startpage studymode sxhkd xinitrc zathura zsh
-git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
-curl -fsSL https://raw.githubusercontent.com/spicetify/spicetify-cli/master/install.sh | sh
-curl -fsSL https://raw.githubusercontent.com/spicetify/spicetify-marketplace/main/resources/install.sh | sh
-spicetify -n apply
