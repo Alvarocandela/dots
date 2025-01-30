516963f2fcfdc147a4a22bb01761d2ceb018a639
Álvaro Candela <alvarocandela47@gmail.com>
2025-01-30 14:08:20 +0100

    added some configs

---
+++ polybar/scripts/playerctl.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+player_status=$(playerctl status 2>/dev/null)
+if [[ $? -eq 0 ]]; then
+    artist="$(playerctl metadata artist)"
+    title="$(playerctl metadata title)"
+    metadata="$artist - ${title%-*}"
+fi
+
+# Foreground color formatting tags are optional
+if [[ $player_status = "Playing" ]]; then
+    echo "%{F#FFFFFF}$metadata%{F-}"
+elif [[ $player_status = "Paused" ]]; then
+    echo "%{F#999}$metadata%{F-}"
+else
+    echo ""
+fi
