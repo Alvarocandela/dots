b24784703828f56657ff53d9775331ee3d53e2ad
Álvaro Candela <alvarocandela47@gmail.com>
2025-11-18 19:01:29 +0100

    Started updating dwm config for use with dwmblocks

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -23,6 +23,7 @@ static const char *const autostart[] = {
   "feh", "--bg-fill", "/home/alvaro/.dots/config/wall/dnord4k_dark.png", NULL,
   "xrandr", "--output", "HDMI-1", "--mode", "2560x1080", "--rate", "100", "--right-of", "eDP-1", NULL,
   "xset", "s", "off", NULL,
+  "picom", NULL,
 	NULL /* terminate */
 };
 
@@ -62,8 +63,6 @@ static const Layout layouts[] = {
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
-
-#define STATUSBAR "dwmblocks"
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
@@ -147,9 +146,7 @@ static const Button buttons[] = {
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	{ ClkStatusText,        0,              Button1,        sigstatusbar,   {.i = 1} },
-	{ ClkStatusText,        0,              Button2,        sigstatusbar,   {.i = 2} },
-	{ ClkStatusText,        0,              Button3,        sigstatusbar,   {.i = 3} },
+	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
